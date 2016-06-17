1898dca051c17e9958f4fdd76e85047c61bbd6e1
Tom Krajnik <tkrajnik@lincoln.ac.uk>
2016-06-17 22:49:14 +0200

    T-test added to benchmarks.

--- src/scripts/evaluate.sh
+++ src/scripts/evaluate.sh
@@ -1,7 +1,9 @@
 #evaluate predition/estimation error on three datasets 
+
 for i in $(seq 0 2);
 do 
 	./scripts/evaluateone.sh data/test_times_$i.txt data/test_data_$i.txt >results/test_$i.txt
+	echo New method performed $(paste results/orig_$i.txt results/test_$i.txt|tr \\t ' '|cut -f 2,4 -d ' '|./scripts/t-test) on testing data $i.  >>results/summary.txt
 done
 gnuplot scripts/draw_results.gnu >results/results.fig
 fig2dev -Lpng -m 5 results/results.fig results/results.png
